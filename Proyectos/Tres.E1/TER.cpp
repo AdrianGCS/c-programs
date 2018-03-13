56120ec2a353706e69ae0c53e74e3fd635a283d6
AdrianGCS <adrian1271997@gmail.com>
2018-03-13 12:14:09 +0100

    Actualización de TER

--- Proyectos/Tres.E1/TER.cpp
+++ Proyectos/Tres.E1/TER.cpp
@@ -2,22 +2,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define Num 5
+#define Num 4
 #define total 3
 //#define Num2 2
 
 int main(){
-    int lugar[total][total];
+    int lugar[total][total]=
+    {   {0,0,0},
+        {0,0,0},
+        {0,0,0}};
+    int jugador = 0;
+    int temporal = 0;
+    int contador=0;
+    int error=1;
+    int ganar=0
 
-    //int tamanio=0;
-    //int fin = total * tamanio;
+  while(ganar>0){
+    while(error=0){
+        error=0;
+        if(jugador>=1){
+            printf("dame un numero \n");
+            scanf("%i", &temporal);
+            if(temporal>=0 or temporal<=10)
+                error=1;
+            if(error=0 and temporal>0 and temporal<=3)
+                if(lugar[0][temporal-1]==0)
+                    lugar[0][temporal-1]=temporal;
+                else
+                    error=1;
+            else
+                if(error=0 and temporal>=4 and temporal<=6)
+                    if(lugar[1][temporal-4]==0)
+                        lugar[1][temporal-4]=temporal;
+                    else
+                        error=1;
+                else
+                    if(error=0 and temporal>=6 and temporal<=9)
+                        if(lugar[2][temporal-6]==0)
+                            lugar[2][temporal-7]=temporal;
+                        else
+                            error=1;
+        }
+    }
 
-    //while(tamanio == 0 or tamanio > 7){
-    //printf("De que tamaño lo quieres \n");
-    //scanf( " %i",&tamanio);
-    //if(tamanio == 0 or tamanio >7)
-    //printf("ERROR repita el numero\n");
-    //}
 
 
     //Este apartado se encarga de dibujar la matriz es decir "El tres en raya"
@@ -25,39 +52,42 @@ int main(){
         //Este apartado dibuja 2 de las 3 lineas
         for(int x = 0; x < Num; x++)
             for(int x = 0; x < total; x++)
-                printf("⏹");
-        printf("⏹");
+                printf("*");
+        printf("*");
         printf("\n");
         //Este se encarga de dibujar los espacios entre las lineas
         for(int W = 0 ; W < total;W++ ){
             for(int z = 0; z < total; z++){
-                printf("⏹ ");
+                printf("* ");
                 //esto metera en el espacio entre líneas 2 una X , una O o un espacio
-                if(z == 2){
-                    lugar[W][z] = 1;
-                    if(lugar[W][z] == 1){
+                if(W == 1){
+                    if(lugar[y][z] == 1){
                         printf("X");}
-                    if(lugar[W][z] == 2)
-                        printf ("O");
                     else
-                        printf(" ");
+                        if(lugar[y][z] == 2)
+                            printf ("O");
+                        else
+                            contador++;
+                    printf("%i", contador);
                 }
                 else
                     printf(" ");
-                printf("  ");}
-            printf("⏹");
+                printf(" ");
+            }
+            printf("*");
             printf("\n");
         }
         //dibuja la línea final
         if(y == total-1){
             for(int x = 0; x < Num; x++)
                 for(int x = 0; x < total; x++)
-                    printf("⏹");
-            printf("⏹");
+                    printf("*");
+            printf("*");
 
             printf("\n");}
 
-
     }
+  }
+
     return EXIT_SUCCESS;
 }
