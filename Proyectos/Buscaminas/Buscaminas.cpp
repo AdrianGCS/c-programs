9d9d5bb3473f1f3bf21ea7787cd405738588bb24
AdrianGCS <adrian1271997@gmail.com>
2018-04-12 14:26:00 +0200

    Buscaminas actualizado, ahora tiene niveles de dificultad

--- Proyectos/Buscaminas/Buscaminas.cpp
+++ Proyectos/Buscaminas/Buscaminas.cpp
@@ -3,43 +3,76 @@
 #include <time.h>
 #include <string.h>
 #define N 10
-#define minas 15
+#define minas 30
 
 void PreguntarDif(int *Dificultad){
     char dificultad;
+//este apartado se encarga de determinar la dificultad de la partida
+    do{
+        printf("Buenos dias Usuario, seleccione un el nivel de dificultad con el que quiere jugar \n");
+        printf("Facil \nNormal\nDificil\n");
+        dificultad=getchar();
+        switch(dificultad){
+            case 'f':
+                dificultad='F';
+                break;
+            case 'n':
+                dificultad='N';
+                break;
+            case 'd':
+                dificultad='D';
+                break;
+            default:
+                break;
+        }
+        switch(dificultad)
+        {
+            case 'F':
+                *Dificultad=3;
+                break;
+            case 'N':
+                *Dificultad=2;
+                break;
+            case 'D':
+                *Dificultad=1;
+                break;
+            default:
+                printf("Error, fallo al insertar la dificultad \n ,");
+                *Dificultad=0;
+        }
+    }while(*Dificultad==0);
+}
 
-    printf("Buenos dias Usuario, seleccione un el nivel de dificultad con el que quiere jugar \n");
-    printf("Facil \n Normal\nDificil\n");
-    scanf("%c",&dificultad);
+void LEminas(int Dificultad, int Lminas){
+    int temporal;
 
+//Este apartado determina la posición de las minas,tiene en cuenta la dificultad
+    srand(time(NULL));
+    for(int W=0;W<minas/Dificultad;W++){
+      do{
+      temporal==rand() % 101;
+      for(int Z=0;Z<=W;Z++)
+          if(Lminas[Z]==temporal)
+              temporal==101;
+      if(temporal!=101)
+          Lminas[W]==temporal;
 
-    switch(dificultad)
-    {
-        case 'F':
-            *Dificultad=1;
-            break;
-        case 'N':
-            *Dificultad=2;
-            break;
-        case 'D':
-            *Dificultad=3;
-            break;
-        default:
-            printf("Error, fallo al insertar la dificultad \n");
-            *Dificultad=0;
+      }while(temporal==101);
+
     }
 
 }
-
 int main(){
 
     int Datos[N][N];
     int Lminas[minas];
-        int Dificultad;
+    int Dificultad;
 
-    bzero(Datos);
-    srand(time(NULL));
+    bzero(Datos,sizeof(Datos));
+    bzero(Lminas,sizeof(Lminas));
     PreguntarDif(&Dificultad);
+    LEminas(Dificultad,&Lminas);
+    printf("%i", Dificultad);
 
     return EXIT_SUCCESS;
 }
