4fecee6ecd2a6543dca31c0e20dd9d404ebe595b
AdrianGCS <adrian1271997@gmail.com>
2018-04-17 11:31:19 +0200

    Mejora del Buscaminas

--- Proyectos/Buscaminas/Buscaminas.cpp
+++ Proyectos/Buscaminas/Buscaminas.cpp
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#define N 10
-#define minas 30
+#define N 20
+#define minas (N*N)
+
 
 void PreguntarDif(int *Dificultad){
     char dificultad;
@@ -28,36 +29,50 @@ void PreguntarDif(int *Dificultad){
         switch(dificultad)
         {
             case 'F':
-                *Dificultad=3;
+                *Dificultad=30;
                 break;
             case 'N':
-                *Dificultad=2;
+                *Dificultad=20;
                 break;
             case 'D':
-                *Dificultad=1;
+                *Dificultad=10;
                 break;
             default:
                 printf("Error, fallo al insertar la dificultad \n ,");
                 *Dificultad=0;
         }
     }while(*Dificultad==0);
+
 }
 
+void Dibujar(int TemDatos[][N]){
+    system("clear");
+    for(int i=0;i<N;i++){
+        for(int x=0;x<N;x++)
+            if(TemDatos[i][x]==0)
+                printf("░ ");
+    printf("\n \n");
+    TemDatos[0][1]=1;
+    }
+
+}
 
 int main(){
 
     int Datos[N][N];
-    int Lminas[minas];
+    int TemDatos[N][N];
+    int Lminas[(minas*30)/100];
     int Dificultad;
     int temporal;
 
     bzero(Datos,sizeof(Datos));
     bzero(Lminas,sizeof(Lminas));
+    bzero(TemDatos,sizeof(TemDatos));
     PreguntarDif(&Dificultad);
 
 //Este apartado determina la posición de las minas,tiene en cuenta la dificultad
     srand(time(NULL));
-    for(int W=0;W<minas/Dificultad;W++){
+    for(int W=0;W<(minas*Dificultad)/100;W++){
       do{
       temporal=rand() % 100 + 1;
       for(int Z=0;Z<=W;Z++)
@@ -68,7 +83,8 @@ int main(){
 
       }while(temporal==101);
     }
-    printf("%i", Dificultad);
+
+    Dibujar(TemDatos);
 
     return EXIT_SUCCESS;
 }
