76a5ac580a4f088a39a3eda588f9275423e7c24e
AdrianGCS <adrian1271997@gmail.com>
2018-05-10 00:42:01 -0500

    tarea

--- 2017/23_func_point/calc.cpp
+++ 2017/23_func_point/calc.cpp
@@ -41,6 +41,7 @@ bool push(int dato, Pila *pila){
 }
 
 int pop(Pila *pila) {
+
     if (pila->cima == 0){
         error = 1;
         return 0;
@@ -49,7 +50,8 @@ int pop(Pila *pila) {
     return pila->data[--pila->cima];
 }
 
-double sum(double op1, double op2) { return op1 + op2; }
+double sum(double op1, double op2) { return op1 + op2; 
+}
 double res(double op1, double op2) { return op1 - op2; }
 double mul(double op1, double op2) { return op1 * op2; }
 double div(double op1, double op2) { return op1 / op2; }
@@ -59,21 +61,58 @@ int main(){
     Pila op, datos;
     double op1, op2;
     char opera;
+    double s; 
+   
     Operacion catalogo[] = {
         {"suma",  &sum},
         {"resta", &res},
-        {"multiplacion", &mul},
+        {"multiplicacion", &mul},
         {"division", &div}
     };
+for(int a=0; a<10; a++){
+    printf("Operando 1: ");
+    scanf(" %lf", &op1);
+    printf("\n");
+    printf("Mete operador: ");
+    scanf(" %c", &opera);
 
-    printf("Operacion: ");
-    scanf(" %lf %c %lf", &op1, &opera, &op2);
+    printf("\n");
+    printf("Operando 2: ");
+    scanf(" %lf", &op2);
+  
+
     push(op2, &datos);
     push(op1, &datos);
+    
+
     switch(opera) {
         case '+':
             push(suma, &op);
+            printf("%.2lf\n", sum(op1, op2));
+            break;
+        case '-':
+            push(resta, &op);
+            printf("%.2lf\n", res(op1, op2));
+
+            break;
+        case '*':
+            push(multi, &op);
+            printf("%.2lf\n", mul(op1, op2));
+
+            break;
+        case '/':
+            push(division, &op);
+            printf("%.2lf\n", div(op1, op2));
+
+            break;
+        default:
+            printf("+ - * /");
             break;
     }
-    return EXIT_SUCCESS;
+    
+    pop(&op);
+}
+ pop(&datos);
+
+   return EXIT_SUCCESS;
 }
